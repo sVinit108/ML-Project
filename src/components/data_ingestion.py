1f998a02fdb6ce69855b25f92b9f6e8ef4f9cdbb
Vinit <sharmavinit.1008@gmail.com>
2023-04-16 13:19:41 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,14 @@
 import os
 import sys
+
 import pandas as pd
-from src.logger import logging
 from dataclasses import dataclass
-from src.exception import CustomException
 from sklearn.model_selection import  train_test_split
+
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
 
 @dataclass
 class DataIngestionConfig:
@@ -44,4 +48,7 @@ class DataIngestion:
         
 if __name__ == '__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
