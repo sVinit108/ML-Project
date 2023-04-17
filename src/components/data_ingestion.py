e7a38450821c49074d4cb48da28304f767a06143
Vinit <sharmavinit.1008@gmail.com>
2023-04-17 12:21:04 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import  train_test_split
 
 from src.logger import logging
 from src.exception import CustomException
+from src.components.model_trainer import ModelTrainerConfig, ModelTrainer
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
 
 
@@ -51,4 +52,7 @@ if __name__ == '__main__':
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
